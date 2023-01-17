e736b9531ff759509eff36c02d5eb6a82e2a1e70
Kacper Hemperek <kacper.hemperek@o2.pl>
2023-01-17 16:07:53 +0100

    change route names

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -11,9 +11,9 @@ export const router = createBrowserRouter(
   createRoutesFromElements(
     <Route element={<Layout />}>
       <Route path='/' element={<Homepage />} />
-      <Route path='/login' element={<Login />} />
-      <Route path='/history' element={<div>History</div>} />
-      <Route path='/statistics' element={<div>Statistics</div>} />
+      <Route path='login' element={<Login />} />
+      <Route path='history' element={<div>History</div>} />
+      <Route path='statistics' element={<div>Statistics</div>} />
     </Route>
   )
 );
--- src/router/routes/Homepage.tsx
+++ src/router/routes/Homepage.tsx
@@ -1,29 +1,11 @@
-import { Link } from 'react-router-dom';
 import { useUser } from '../../hooks/useUser';
-import Loadingpage from './Loadingpage';
 
 function Homepage() {
-  const { user, loading, error } = useUser();
-
-  if (loading) {
-    return <Loadingpage />;
-  }
-
-  if (error) {
-    return <div>{error}</div>;
-  }
-
-  if (!user) {
-    return (
-      <div className='x-auto'>
-        <Link to={'/login'}>Login</Link>
-      </div>
-    );
-  }
+  const { user } = useUser();
 
   return (
     <div className='mx-auto flex max-w-fit flex-col text-center'>
-      {user.email}
+      {user?.email}
     </div>
   );
 }
