d90188683461025d70c56c031ee3576d43b1b0cf
Kacper Hemperek <kacper.hemperek@o2.pl>
2023-03-11 23:05:28 +0100

    update user optimistically

--- src/hooks/useLogout.ts
+++ src/hooks/useLogout.ts
@@ -1,12 +1,31 @@
+import { queryClient } from '@/App';
+import { User } from '@/context/UserContext';
 import { fetchFn } from '@/utils/fetchFunction';
 import { useMutation } from '@tanstack/react-query';
 
 export function useLogout() {
   return useMutation({
     mutationFn: async () => {
-      const user = await fetchFn({ url: '/api/session', method: 'DELETE' });
+      await fetchFn({ url: '/api/session', method: 'DELETE' });
+    },
+    onMutate: async () => {
+      await queryClient.cancelQueries({ queryKey: ['user'] });
 
-      console.log(user);
+      const prevUser = queryClient.getQueryData<User | null>(['user']);
+
+      console.log({ prevUser });
+
+      queryClient.setQueryData(['user'], null);
+
+      return { prevUser };
+    },
+    onError: (error, _, context) => {
+      console.log('error when logging out');
+
+      queryClient.setQueryData(['user'], context?.prevUser);
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: ['user'] });
     },
   });
 }
